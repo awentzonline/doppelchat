21df5dfac60dcaccc6f5608272a8428c5d048cf5
Adam Wentz <adam@adamwentz.com>
2015-10-19 18:28:54 -0500

    add docker config

--- src/server/run.js
+++ src/server/run.js
@@ -3,6 +3,12 @@ var serverPort = process.env.PEER_PORT || 9000;
 var serverPath = process.env.PEER_PATH || '/';
 var allowDiscovery = process.env.PEER_DISCOVERY !== 'off';
 
+console.log([
+  'Starting server key:', serverKey,
+  ', port:', serverPort,
+  ', path:', serverPath].join('')
+);
+
 var PeerServer = require('peer').PeerServer;
 var server = PeerServer({
   key: serverKey,
